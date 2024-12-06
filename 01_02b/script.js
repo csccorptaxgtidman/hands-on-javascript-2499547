3c40694bc432145a81a31d023b8d2d7ff889e4f2
csccorptaxgtidman <gerald.tidman@cscglobal.com>
2024-12-06 18:27:20 +0000

    course work

--- 01_02b/script.js
+++ 01_02b/script.js
@@ -8,6 +8,29 @@ import data from "./data.js";
 
 const mainContent = document.querySelector(".main-content");
 
+const buildImage = (imgData) => {
+
+  let srcset = `${imgData.urls.full} ${imgData.width}w`;
+  if(imgData.urls.regular) {
+    srcset = srcset + `, ${imgData.urls.regular} 1080w`;
+  }
+  if(imgData.urls.small) {
+    srcset = srcset + `, ${imgData.urls.small} 400w`;
+  }
+
+  const img = `
+  <img
+        srcset="${srcset}"
+        sizes="(max-width: 450px) 400px, (max-width: 800) 1080px"
+        src="${imgData.urls.regular}"
+        width="${imgData.width}"
+        height="${imgData.height}"
+        alt="${imgData.description}"
+        loading="lazy"
+      />`;
+  return img;
+};
+
 const getDate = (imgData) => {
   const date = new Date(imgData.created_at);
   const niceDate = date.toLocaleString("default", {
@@ -23,19 +46,7 @@ const Card = (data) => {
   
   const markup = `
     <figure class="image">
-      <img
-        srcset="
-          ${imgData.urls.full} ${imgData.width}w,
-          ${imgData.urls.regular} 1080w,
-          ${imgData.urls.small} 400w
-        "
-        sizes="(max-width: 450px) 400px, (max-width: 800) 1080px"
-        src="${imgData.urls.regular}"
-        width="${imgData.width}"
-        height="${imgData.height}"
-        alt="${imgData.description}"
-        loading="lazy"
-      />
+      ${buildImage(imgData)}
       <figcaption class="image__caption">
         <h3 class="image__title">${imgData.description}</h3>
         <div class="image__meta">
