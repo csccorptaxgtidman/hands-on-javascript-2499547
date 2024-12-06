b7107c6046a14fe5503fed072e5ec2ca31a06d0a
csccorptaxgtidman <gerald.tidman@cscglobal.com>
2024-12-06 22:01:51 +0000

    course 03_02

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 export default {
   // Get API key from https://home.openweathermap.org/users/sign_up
-  appid: [OpenWeatherAPIkey],
+  appid: "fdfbc75f3de6dd8c3b7c59509451ebf6",
   units: "metric",
-  location: "carpinteria,ca,us",
+  location: "little elm,tx,us",
 };
